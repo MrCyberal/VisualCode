d695a9c749708d3100eae084bf7da42f1a15bbfc
MrCyberal <alain.vano@gmail.com>
2022-02-14 11:43:38 +0100

    .

--- Pendu/Main.py
+++ Pendu/Main.py
@@ -3,35 +3,36 @@
 # Chaque fois qu'une lettre est trouvée, l'etoile est remplacee par la lettre ex : *o**o** pour bonjour si o est trouve
 # On affiche aussi le nombre de chances restantes a chaque tour
 
-from ast import And
+# from ast import And
+# from ntpath import join
 from ntpath import join
 import random
-from re import I
+# from re import I
 from Sequence_pendu import graph_pendu 
 
 
 # Liste des mots du pendu
 liste_mots = [
-    "armoire",
-    "boucle",
-    "buisson",
-    "bureau",
-    "chaise",
-    "carton",
+    "armoiree",
+    "bouclee",
+    "buissoni",
+    "beureau",
+    "chaisee",
+    "cartonn",
     "couteau",
     "fichier",
     "garage",
-    "glace",
-    "journal",
+    "glacee",
+    "journale",
     "kiwi",
-    "lampe",
-    "liste",
-    "montagne",
+    "lampee",
+    "listee",
+    "montagnee",
     "remise",
     "sandale",
-    "taxi",
-    "vampire",
-    "volant",
+    "taxia",
+    "vampiree",
+    "volantt",
 ]
 
 # on prend un mots de la liste de maniere aleatoire
@@ -45,9 +46,9 @@ nb_lettres_trouvees = int(1)
 Longueur_mot = len(mot_a_trouver)
 x=0
 print("le mot a trouver : ", Longueur_mot*"*")
-
  
 lettres_trouvees = ["*"] * Longueur_mot
+convert_lettres_trouvees = " ".join(lettres_trouvees)
 
 while (nb_essai < nb_chances) and (nb_lettres_trouvees <= Longueur_mot):
 
@@ -67,13 +68,14 @@ while (nb_essai < nb_chances) and (nb_lettres_trouvees <= Longueur_mot):
             if lettre == choix_lettre :
                 nb_lettres_trouvees += 1
                 lettres_trouvees[position] = choix_lettre
-                print(lettres_trouvees)
-
+        convert_lettres_trouvees = " ".join(lettres_trouvees)      
+        print(convert_lettres_trouvees)     
+    
     else :
         graph_pendu(nb_essai)
         nb_essai+=1
-        print(lettres_trouvees)
-        
+        print(convert_lettres_trouvees)
+       
 if (nb_lettres_trouvees <= Longueur_mot)== False:
 
     print(" Vous avez trouve!!")
