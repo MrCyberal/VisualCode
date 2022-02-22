8e7782cbb4e5bc1539b05b37256a5199aa252d7e
MrCyberal <alain.vano@gmail.com>
2022-02-22 12:23:58 +0100

    .

--- Exercices Clavier/index.js
+++ Exercices Clavier/index.js
@@ -3,10 +3,13 @@ const boutons = document.getElementsByTagName("button");
 
 let boutonsCpt = boutons.length;
 
-for (var i = 0; i < boutonsCpt; i += 1) {
-boutons[i].onclick = function(e) {
 
-    document.location.assign("Exercice1.html")
+for (var i = 0; i < boutonsCpt; i += 1) {
+
+    boutons[i].onclick = function(e) {
+
+        let idbouton = e.target.id
+        document.location.assign("Exercice1.html")
     
 
 //alert(this.id);
