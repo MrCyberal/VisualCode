132f0cafb3b0453eedc72a75135aa4456ed8cf50
MrCyberal <alain.vano@gmail.com>
2022-03-09 10:09:55 +0100

    .

--- Exercices Clavier/main.js
+++ Exercices Clavier/main.js
@@ -1,12 +1,12 @@
 let lesson = [["partir en vacance","des voitures","il est tard","un parapluie","prendre et donner","une histoire courte","je ne suis pas malade","l'information est fausse"],
               ["179/83","123*34.90","7294+2309","1023.3-985.20","10255*96.33-8","751+654*59","4376.91/23.2","1385+12.36-9","72006952+253916","9836120/895.64","1956.36*21.42"],
               ["Manon","La victoire","Fred Dino","Xenomorphe","Louis Pasteur","Je Mets Une Majuscule"],
-              ["Le point d'exclamation: !","Le point d'interrogation: ?","La barre oblique: /","Le point: .","La virgule: ,","Le point virgule: ;","l'apostrophe: '",'Le guillement"',
+              ["Le point d'exclamation: !","Le point d'interrogation: ?","La barre oblique: /","Le point: .","La virgule: ,","Le point virgule: ;","L'apostrophe: '",'Le guillement: "',
                "Le trait-union: -"],
-              ["Le 'e' accent aigu: é","le 'e' accent grave: è","Le 'e' accent circonflexe: ê","le tréma sur le 'e': ë","LE 'œ' s'obtient grâce à Alt + 0156","Un œuf"]
+              ["Le 'e' accent aigu: é","Le 'e' accent grave: è","Le 'e' accent circonflexe: ê","Le tréma sur le 'e': ë","LE 'œ' s'obtient grâce à Alt + 0156","Un œuf"]
               ["La lumière","L'obscurité","Des réactions inouïes","Une leçon","Un reçu","La fête","Une bête","Le théâtre","Un château","Affûter son esprit"],
-              ["Un amas d'étoile","La fête à l'école","Noël et anaïs","Le garçon médisant","l'épée émousée","L'après-midi à Paris","Il écrit à la famille","Je suis là-bas !"],
-              ["Le diése ou hashtag: #","l'arobase ou at: @","Les parenthèses: ()","Les crochets: []","Les accolades: {}","La barre oblique inverse ou backslash: \\","Le tilde: ~",
+              ["Un amas d'étoile","La fête à l'école","Noël et anaïs","Le garçon médisant","L'épée émousée","L'après-midi à Paris","Il écrit à la famille","Je suis là-bas !"],
+              ["Le diése ou hashtag: #","L'arobase ou at: @","Les parenthèses: ()","Les crochets: []","Les accolades: {}","La barre oblique inverse ou backslash: \\","Le tilde: ~",
                "La verticale: |"],
               ["Le 'http://' n'a plus besoin d'être tapé dans un navigateur internet","L'adresse mail de Jean-Pierre est jp.duhon1962@mail.fr",]
             ];
@@ -18,7 +18,7 @@ const BoutonVerif = document.querySelector("#boutonVerif");
 const cadreExercice = document.querySelector(".cadreExercice");
 const exerciceFini = document.querySelector(".exerciceFini");
 const boutonMenu = document.querySelector(".retourMenu");
-const textNbrMot = document.querySelector("#nbrMot");
+const textNbrMot = document.querySelector(".nbrMot");
 const TextTimer = document.querySelector("#timer");
 
 let cpt = 0;
@@ -46,6 +46,34 @@ function choixExo(valStore) {
             return cptarray = 1;
         break;
     
+        case "lesson3" :
+            CompteRebour();
+            return cptarray = 2;
+        break;
+
+        case "lesson4" :
+            CompteRebour();
+            return cptarray = 3;
+        break;
+
+        case "lesson5" :
+            CompteRebour();
+            return cptarray = 4;
+        break;
+
+        case "lesson6" :
+            CompteRebour();
+            return cptarray = 5;
+        break;
+
+        case "lesson7" :
+            CompteRebour();
+            return cptarray = 6;
+        break;
+
+        case "lesson8" :
+            CompteRebour();
+            return cptarray = 7;
         default:
             null;
 
@@ -95,6 +123,7 @@ function InjectionText(cpt) {
 
     if (cpt < lesson[cptarray].length) {
         textaTaper.innerHTML = lesson[cptarray][cpt];
+        textNbrMot.textContent = cpt;
         textaChecker.focus();
         textaChecker.value=null;
 
@@ -111,8 +140,7 @@ function InjectionText(cpt) {
 function AjoutZero(num){
 
     return num < 10 ? "0" + num : num; 
-    
-
+ 
 }
 
 function CompteRebour() {
